b40879171100a20cd2d8cfd6c9bcded96758a5cb
kanishk466 <kanishkas466@gmail.com>
2024-07-03 14:55:15 +0530

    added user and video model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import dotenv from "dotenv"
-
+import {app} from "./app.js"
 import connectDB   from "./db/db.js"
 
 dotenv.config()
 connectDB()
-.then()
+.then(app.listen(process.env.PORT , (err)=>{
+    if(err) console.log("Error in server Setup")
+    console.log(`Server is running on port ${process.env.PORT}`)
+} ))
 .catch(err => console.log("Mongo Db connection failed " ,err))
