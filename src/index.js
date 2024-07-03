036053d90dac0472d538761e53273e748df1e9ff
kanishk466 <kanishkas466@gmail.com>
2024-07-03 12:26:00 +0530

    added code of utils

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import dotenv from "dotenv"
 import connectDB   from "./db/db.js"
 
 dotenv.config()
-connectDB();
+connectDB()
+.then()
+.catch(err => console.log("Mongo Db connection failed " ,err))
